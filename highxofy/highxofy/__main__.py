f2c3c37cf47a44f8e9e28123c9743daea27c5da9
gnkm <genki.matsunaga@gmail.com>
2021-11-13 13:33:09 +0900

    Add main algorithm

--- highxofy/highxofy/__main__.py
+++ highxofy/highxofy/__main__.py
@@ -4,19 +4,36 @@ import sys
 import pandas as pd
 import icecream  # noqa: F401  # pylint: disable=unused-import
 
+from highxofy import calculator
+
 
 def main():
+    """Dump result file in output directory.
+    """
     parser = make_parser()
     args = parser.parse_args()
-    csv_file = args.csv_file
+    input_file = args.input_file
+    output_file = args.output_file
+    df_org = pd.read_csv(input_file, encoding='sjis')
+    df_calculated = calculator.calculate(df_org)
+    df_calculated.to_csv(output_file)
     sys.exit()
 
 
-def make_parser():
+def make_parser() -> argparse.ArgumentParser:
+    """Return ArgumentParser.
+
+    Returns:
+        argparse.ArgumentParser: parser defined args
+    """
     parser = argparse.ArgumentParser(description='Calculate high x of y.')
     parser.add_argument(
-        'csv_file',
+        'input_file',
         help='original data'
+    )
+    parser.add_argument(
+        'output_file',
+        help='calculated result file'
     )
 
     return parser
