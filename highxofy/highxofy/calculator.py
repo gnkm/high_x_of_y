9c26e2c1d43baef3093469f7d63b46988759411d
gnkm <genki.matsunaga@gmail.com>
2021-11-13 13:47:02 +0900

    Add calculator module

---
+++ highxofy/highxofy/calculator.py
@@ -0,0 +1,18 @@
+"""Calculate high x of y.
+"""
+
+import pandas as pd
+
+
+def calculate(df_org: pd.DataFrame) -> pd.DataFrame:
+    """Return dataframe contain high x of y result.
+
+    Args:
+        df_org (pd.DataFrame): Original data. i.e. historical data.
+
+    Returns:
+        pd.DataFrame: dataframe contain high x of y result.
+    """
+    df = df_org
+
+    return df
