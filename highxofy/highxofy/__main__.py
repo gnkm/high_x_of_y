170a1f9c470647bd90832b316fb6de4eb8b18827
gnkm <genki.matsunaga@gmail.com>
2021-11-13 09:24:04 +0900

    Add argparser

--- highxofy/highxofy/__main__.py
+++ highxofy/highxofy/__main__.py
@@ -1,10 +1,25 @@
+import argparse
 import sys
 
+import pandas as pd
 import icecream  # noqa: F401  # pylint: disable=unused-import
 
 
 def main():
+    parser = make_parser()
+    args = parser.parse_args()
+    csv_file = args.csv_file
     sys.exit()
+
+
+def make_parser():
+    parser = argparse.ArgumentParser(description='Calculate high x of y.')
+    parser.add_argument(
+        'csv_file',
+        help='original data'
+    )
+
+    return parser
 
 
 if __name__ == '__main__':
